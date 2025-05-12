564d3c6bef8a7a715c1f7cf37453283c2207d391
Chandan Kumar <Ck598364@gmail.com>
2025-05-12 12:15:46 +0530

    Create SyntaxFunction.java
    1.  When we using " int " data type then we will use "return" statement.
    2.   If when we will  use "void" then no need to use "return " statement.
    3.   only one main method can we used in program. 
    4.   in one class we wcan use multiple functions in the program.

---
+++ SyntaxFunction.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class SyntaxFunction{
+
+  public static void greet(){
+    System.out.print("Hey.. my name is chandan:");
+    System.out.print("Hey.. my name is chandan:");
+    System.out.print("Hey.. my name is chandan:");
+    System.out.print("Hey.. my name is chandan:");
+  }
+
+  public static void calculatesum(){
+    Scanner sc =new Scanner(System.in);
+    System.out.print("Enter your first number:");
+    int a =sc.nextInt();
+    System.out.print("Enter your second number:");
+    int b = sc.nextInt();
+    int sum = a+b;
+    System.out.println("Your total sum si:" +sum);
+  }
+  public static void main(String [] args){
+    greet();
+    calculatesum();
+  }
+}
+
+
