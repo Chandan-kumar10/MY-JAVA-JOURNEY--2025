539ea08a77b9b8099b2aae431bab4c10fe75df2a
Chandan Kumar <Ck598364@gmail.com>
2025-05-15 09:20:08 +0530

    Create DataTypes.java

---
+++ DataTypes.java
@@ -0,0 +1,16 @@
+class Datatype {
+
+    public static void main(String[] agrs) {
+
+        int n = 234;
+        short ns = 3423;
+        byte bs = 127;
+        long l = 34564;
+        float f = 3.4f;
+        double d = 3.5;
+        char c = 'b';
+        boolean s = false;
+        String str = "chandan";
+
+    }
+}
