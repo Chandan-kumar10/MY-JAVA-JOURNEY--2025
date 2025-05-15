3cbea916f0a0526534fb4e84abbfa25cf459cf3e
Chandan Kumar <Ck598364@gmail.com>
2025-05-15 09:17:55 +0530

    Create DataTypes.java

---
+++ DataTypes.java
@@ -0,0 +1,18 @@
+lass FindProduct {
+
+    public int product() {
+        int a = 8;
+        int b = 6;
+        int s = a * b;
+        System.out.println("total product is:" + s);
+        return s;
+    }
+
+    public static void main(String[] agrs) {
+
+        FindProduct obj = new FindProduct();
+        obj.product();
+
+    }
+}
+// output is:---   48.
