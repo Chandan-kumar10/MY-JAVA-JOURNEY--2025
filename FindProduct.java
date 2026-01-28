2d3f11ef8f3b2dee7f19b437c37ae42856ca8fe1
Chandan Kumar <Ck598364@gmail.com>
2026-01-28 20:31:59 +0530

    Update FindProduct.java

--- FindProduct.java
+++ FindProduct.java
@@ -3,14 +3,14 @@ lass FindProduct {
     public int product() {
         int a = 8;
         int b = 6;
-        int s = a * b;
+        int s = a*b;
         System.out.println("total product is:" + s);
         return s;
     }
 
     public static void main(String[] agrs) {
 
-        FindProduct obj = new FindProduct();
+        FindProduct obj = new Findproduct();
         obj.product();
 
     }
