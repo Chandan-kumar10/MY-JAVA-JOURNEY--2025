68c14412c3171a81e8a3b0a1851d46a99fc3ae95
Chandan Kumar <Ck598364@gmail.com>
2026-02-16 18:43:25 +0530

    Add Wrappermethod class to sum two integers

---
+++ Wrappermethod.java
@@ -0,0 +1,12 @@
+public class Wrappermethod{
+
+public static void main(String[]args){
+
+  String str = "1234";
+  String str1 = "1234";
+  int a = Integer.parseInt(str);
+  int b = Integer.parseInt(str1);
+  int c = a+b;
+  System.out.println(c);
+}
+}
